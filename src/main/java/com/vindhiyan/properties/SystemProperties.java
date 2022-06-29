05f263b767588420f962ca7798bbd2594f2a5c04
Premraj-Murugaraj <premraj.murugaraj@yahoo.com.sg>
2022-06-29 17:27:24 +0800

    refactor utils

--- src/main/java/com/vindhiyan/properties/SystemProperties.java
+++ src/main/java/com/vindhiyan/properties/SystemProperties.java
@@ -5,7 +5,7 @@ import com.vindhiyan.drivers.Browser;
 import com.vindhiyan.drivers.Target;
 
 public final class SystemProperties {
-    private static final String DEFAULT_ENV = System.getProperty("defaultEnv", "dev");
+    private static final String DEFAULT_ENV = System.getProperty("defaultEnv", "heroku");
     public static final String ENV = System.getProperty("env", DEFAULT_ENV);
     public static final String TARGET = System.getProperty("target", Target.UI.name().toLowerCase());
     public static final String BROWSER = System.getProperty("browser", Browser.CHROME.name().toLowerCase());
